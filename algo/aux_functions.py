correct computation of sample standardization

algo/aux_functions.py
@@ -43,7 +43,10 @@ def batch_standardize(array):
         return (array-np.mean(array, axis=0))/np.std(array, axis=0)
 
 def standardize_sample(array, mean, std):
-    return (array-mean)/std
+    if std.all()==False:
+        return array
+    else:
+        return (array-mean)/std
 
 def re_standardize_sample(array, mean, std):
     return array*std + mean