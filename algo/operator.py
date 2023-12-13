a3cd5f0adc3fedf24965dc08e12436dee529064d
Christoph Schrade <schrade.christoph@gmail.com>
2023-12-13 10:07:13 +0100

    change type of operator forecast returns to float

--- algo/operator.py
+++ algo/operator.py
@@ -132,7 +132,7 @@ class Operator(util.OperatorBase):
                 power_forecast = self.create_power_forecast(new_weather_data)
                 self.weather_same_timestamp = []
                 if True:  #TODO: Implement on conditions that ensures relatively good output. (How much data does one need for good training?)
-                    print("PV-Operator-Output:", [{'timestamp':timestamp.strftime('%Y-%m-%d %X')+'Z', 'value': probability} for timestamp, probability in power_forecast])
-                    return [{'timestamp':timestamp.strftime('%Y-%m-%dT%X')+'Z', 'value': probability} for timestamp, probability in power_forecast]
+                    print("PV-Operator-Output:", [{'timestamp':timestamp.strftime('%Y-%m-%d %X')+'Z', 'value': float(forecast)} for timestamp, forecast in power_forecast])
+                    return [{'timestamp':timestamp.strftime('%Y-%m-%dT%X')+'Z', 'value': float(forecast)} for timestamp, forecast in power_forecast]
         elif selector == 'power_func':
             self.run_new_power(data)
