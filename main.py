589e6d498735c21d3883d579029688b9d62b2873
c-schrade <schrade.christoph@gmail.com>
2026-01-15 15:14:11 +0100

    set negative predictions to 0

--- main.py
+++ main.py
@@ -146,7 +146,8 @@ class Operator(OperatorBase):
                 std_new_weather_input = aux_functions.standardize_sample(new_weather_input, self.weather_mean, self.weather_std)
                 try:
                     model_output = self.model.predict(std_new_weather_input.reshape(1,-1))
-                    power_forecast.append((new_weather_forecasted_for[i],aux_functions.re_standardize_sample(model_output, self.power_mean, self.power_std)))
+                    re_standardized_prediction = aux_functions.re_standardize_sample(model_output, self.power_mean, self.power_std)
+                    power_forecast.append((new_weather_forecasted_for[i],max(0, re_standardized_prediction)))
                 except NotFittedError:
                     logger.info("Model not fitted yet.")
         return power_forecast
